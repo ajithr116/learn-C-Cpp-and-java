8dce2605f69b6a783615dbf056f2d6398db73f5d
Ajith R <92302498+AJITH1359@users.noreply.github.com>
2021-11-01 16:30:28 +0530

    Add files via upload

---
+++ 20 pointer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main () {
+   int  var;
+
+   cout << "Address of var1 variable: ";
+   cout << &var << endl;
+
+   cout << "Address of var variable: ";
+   cout << var<< endl;
+
+   return 0;
+}
